5ff10c49869f870d28494a1190f3e0c5cd81c71f
chenjianhua <jianhua.chen@szkunton.com>
2019-11-18 16:04:31 +0800

    添加travis.yml

--- pkg/golang/goroutineLearn/memoryLeak_test.go
+++ pkg/golang/goroutineLearn/memoryLeak_test.go
@@ -1,1 +1,19 @@
 package goroutineLearn
+
+import (
+	"testing"
+	"time"
+)
+
+func TestLeak(t *testing.T) {
+	go t.Log("你好, 并发!") // 干活的
+
+	// 10000: 80M左右
+	for i := 0; i < 10000; i++ {
+		go func() { <-make(chan int) } () // 滥竽充数的, Goroutine 泄露
+	}
+	go func() {} () // 滥竽充数的, 但不是 Goroutine 泄露
+
+	time.Sleep(30 * time.Second)
+	t.Log("Done")
+}
--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -10,7 +10,7 @@ var ClusterClient *redis.ClusterClient
 
 func NewRedis() {
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr:     "192.168.137.128:6379",
+		Addr:     "192.168.137.129:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 		PoolSize: 20 * runtime.NumCPU(),
