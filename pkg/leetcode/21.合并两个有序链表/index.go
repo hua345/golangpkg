3559cafa7ebbf25782642c50cae07b19c497d64c
chenjianhua <jianhua.chen@szkunton.com>
2020-07-24 10:50:06 +0800

    leetcode

--- pkg/leetcode/21.合并两个有序链表/index.go
+++ pkg/leetcode/21.合并两个有序链表/index.go
@@ -6,34 +6,70 @@ type ListNode struct {
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	return mergeTwoLists2(l1, l2)
+}
+func mergeTwoLists1(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
 	node1 := l1
 	node2 := l2
 	var result *ListNode
-	if l1.Val > l2.Val {
-		result = l2
+	if l1.Val <= l2.Val {
+		result = &ListNode{l1.Val, nil}
+		node1 = l1.Next
 	} else {
-		result = l1
+		result = &ListNode{l2.Val, nil}
+		node2 = l2.Next
 	}
 	resultNode := result
-	for node1 != nil {
-		if node1.Next != nil {
-			if node1.Next.Val > node2.Next.Val {
-				resultNode.Next = node2.Next
-				node2 = node2.Next
-			} else {
-				resultNode.Next = node1.Next
-				node1 = node1.Next
-			}
-			resultNode = resultNode.Next
+	for node1 != nil && node2 != nil {
+		if node1.Val <= node2.Val {
+			resultNode.Next = &ListNode{node1.Val, nil}
+			node1 = node1.Next
 		} else {
-			resultNode.Next = node2.Next
+			resultNode.Next = &ListNode{node2.Val, nil}
 			node2 = node2.Next
-			resultNode = resultNode.Next
-			break
 		}
+		resultNode = resultNode.Next
+	}
+	if node1 != nil {
+		resultNode.Next = node1
 	}
 	if node2 != nil {
 		resultNode.Next = node2
 	}
-	return resultNode
+	return result
+}
+func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	dummyHead := new(ListNode)
+	head := dummyHead
+	dummyHead.Next = head
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			head.Next = l1
+			head = head.Next
+			l1 = l1.Next
+		} else {
+			head.Next = l2
+			l2 = l2.Next
+			head = head.Next
+		}
+	}
+	if l1 == nil {
+		head.Next = l2
+	}
+	if l2 == nil {
+		head.Next = l1
+	}
+	return dummyHead.Next
 }
--- pkg/leetcode/21.合并两个有序链表/index_test.go
+++ pkg/leetcode/21.合并两个有序链表/index_test.go
@@ -3,23 +3,44 @@ package main
 import "testing"
 
 func TestMergeTwoLists(t *testing.T) {
-	t.Run("测试合并两个有序链表", func(t *testing.T) {
+	t.Run("测试[1,2,4],[1,3,4]合并两个有序链表", func(t *testing.T) {
 		l1 := &ListNode{1, nil}
 		l1.Next = &ListNode{2, nil}
 		l1.Next.Next = &ListNode{4, nil}
 		l2 := &ListNode{1, nil}
 		l2.Next = &ListNode{3, nil}
 		l2.Next.Next = &ListNode{4, nil}
-
 		temp := mergeTwoLists(l1, l2)
-
 		for temp != nil && temp.Next != nil {
 			if temp.Val > temp.Next.Val {
-				t.Error("测试合并两个有序链表失败")
+				t.Error("测试[1,2,4],[1,3,4]合并两个有序链表失败")
 				break
 			}
 			temp = temp.Next
 		}
-
+	})
+	t.Run("测试[],[]合并两个有序链表", func(t *testing.T) {
+		var l1 *ListNode
+		var l2 *ListNode
+		temp := mergeTwoLists(l1, l2)
+		for temp != nil && temp.Next != nil {
+			if temp.Val > temp.Next.Val {
+				t.Error("测试[],[]合并两个有序链表失败")
+				break
+			}
+			temp = temp.Next
+		}
+	})
+	t.Run("测试[2],[1]合并两个有序链表", func(t *testing.T) {
+		l1 := &ListNode{2, nil}
+		l2 := &ListNode{1, nil}
+		temp := mergeTwoLists(l1, l2)
+		for temp != nil && temp.Next != nil {
+			if temp.Val > temp.Next.Val {
+				t.Error("测试[2],[1]合并两个有序链表失败")
+				break
+			}
+			temp = temp.Next
+		}
 	})
 }
